1eccb6ac0d0663a3dafc1889b48084ff65bd08a7
Alex Tokarev <Alex42ff@gmail.com>
2026-02-10 01:34:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,39 @@
-const CACHE_NAME = 'dumaguete-emergency-v2';
+const CACHE_NAME = 'dumaguete-emergency-v3';
 const urlsToCache = [
-  './dumaguete-emergency.html',
-  './manifest.json'
+  '/dumarescue/',
+  '/dumarescue/index.html',
+  '/dumarescue/manifest.json'
 ];
 
-// Install Service Worker
+// Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-// Activate Service Worker
+// Activate
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
+    caches.keys().then((names) =>
+      Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
           }
         })
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
 
-// Fetch from cache, fallback to network
+// Fetch
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+      .then((response) => response || fetch(event.request))
   );
 });
